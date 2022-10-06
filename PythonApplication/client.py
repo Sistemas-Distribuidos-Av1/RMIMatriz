8ffcc5423ed0dd28638c14b55228acd345c8fdd7
nysalvi <nysalvi20@gmail.com>
2022-10-06 18:00:59 -0300

    Matriz Linear com numpy

--- PythonApplication/client.py
+++ PythonApplication/client.py
@@ -1,11 +1,16 @@
+import Pyro5.errors
 import Pyro5.api
 
 def main():
-    print("Running RMI client...")
-    print("\tGetting remote object...")
-    calc = Pyro5.api.Proxy("PYRONAME:example.calculator")
-    print("test")
-
+    #print("Running RMI client...")
+    #print("\tGetting remote object...")
+    #calc = Pyro5.api.Proxy("PYRONAME:Calculadora")
+    uri = input("Type the Pyro uri: ").strip()
+    calc = Pyro5.api.Proxy(uri)
+    try:
+        calc.printar()
+    except Exception:
+        print(Pyro5.errors.get_pyro_traceback())
 
 if __name__ == "__main__":
     main()
