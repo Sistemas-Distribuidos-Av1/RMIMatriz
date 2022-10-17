f2b465202e2ecf7b1e1048b166cb9bca719c8917
nysalvi <nysalvi20@gmail.com>
2022-10-17 15:48:58 -0300

    Arrumado thread

--- PythonApplication/MatrizThread.py
+++ PythonApplication/MatrizThread.py
@@ -7,17 +7,19 @@ import time
 if __name__ == "__main__":                
     A = Matriz.carregaMatriz(Matriz, "matA.txt")
     B = Matriz.carregaMatriz(Matriz, "matB.txt")        
-    #matrizFinal = np.ndarray([len(A), len(B[0])], dtype='float')    
-    matrizFinal = []    
+    matrizFinal = np.ndarray([len(A), len(B[0])], dtype='float')    
+    #matrizFinal = []    
     #processos = []
 
+    #queue = Queue()
     start_timer = time.time()
-    queue = Queue()
 
     with Pool() as pool:
         for i in range (8):
-            matrizFinal.append(pool.starmap(Matriz.mul, [(Matriz, A[i * 512 : i * 512 + 512,], B[:])]))
-            #matrizFinal[i * 512: i * 512 + 512,] = pool.starmap(Matriz.mul, [(Matriz, A[i * 512 : i * 512 + 512,], B[:])])            
+            #matrizFinal.append(pool.starmap(Matriz.mul, [(Matriz, A[i * 512 : i * 512 + 512,], B[:])]))
+            #matrizFinal.append(pool.starmap(Matriz.mul, [(Matriz, A[i * 4 : i * 4 + 4,], B[:])]))
+            matrizFinal[i * 512: i * 512 + 512,] = pool.starmap(Matriz.mul, [(Matriz, A[i * 512 : i * 512 + 512,], B[:])]).pop()            
+            #matrizFinal[i * 4: i * 4 + 4,] = pool.starmap(Matriz.mul, [(Matriz, A[i * 4 : i * 4 + 4,], B[:])]).pop()            
         #for result in pool.starmap(Matriz.mul, [(Matriz, A[i : i + 512,], B[:])]):
         #    print()
     """
@@ -32,6 +34,7 @@ if __name__ == "__main__":
     """
     end_timer = time.time() - start_timer   
     print("Passou")
+    #print(matrizFinal)
     """
     for x in range(8):
         t = x * 512
